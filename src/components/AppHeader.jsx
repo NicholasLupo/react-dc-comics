d00cfc4a93fda752f344349d1daabd0feed04110
NicholasLupo <nicholaslupo14@gmail.com>
2026-04-15 16:24:20 +0200

    add logo to AppHeader and populate list items dynamically

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,29 @@
+import LogoImg from "../assets/img/dc-logo.png"
+
 export default function AppHeader() {
+
+    const options = [
+        'CHARACTERS',
+        'COMICS',
+        'MOVIES',
+        'TV',
+        'GAMES',
+        'COLLECTIBLES',
+        'VIDEOS',
+        'FANS',
+        'NEWS',
+        'SHOP'
+    ]
+
+    const ul = document.getElementById('list');
+
+    const element = options.forEach(function (option) {
+
+        const li = document.createElement('li');
+        li.innerText = option
+        ul.appendChild(li)
+
+    })
 
 
     return (
@@ -6,7 +31,12 @@ export default function AppHeader() {
         <header>
             <div className="container">
                 <div className="row">
-                    header
+                    <div className="logo">
+                        <img src={LogoImg} alt="" />
+                    </div>
+                    <div className="selector">
+                        <ul id="list" className="list-group list-group-horizontal"></ul>
+                    </div>
                 </div>
             </div>
         </header>
