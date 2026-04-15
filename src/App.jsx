e1f41b15e820dce77e47095ad5a58fef217e9384
NicholasLupo <nicholaslupo14@gmail.com>
2026-04-15 15:27:59 +0200

    add 3 components and update index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-
+import AppFooter from "./components/AppFooter"
+import AppHeader from "./components/AppHeader"
+import AppMain from "./components/AppMain"
 
 function App() {
 
   return (
     <>
-
+      <AppHeader />
+      <AppMain />
+      <AppFooter />
     </>
   )
 }
