c5330a27aa658ff683b76f704b0e4eb6b4745bf7
NicholasLupo <nicholaslupo14@gmail.com>
2026-04-15 18:25:28 +0200

    styling upper main

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -4,15 +4,21 @@ export default function AppMain() {
     return (
 
         <main>
-            <div className="container">
-                <div id='upper' className="row">
-                    boh
+            <section id="backOne">
+                <div className="container">
+                    <div id='upper' className="row">
+                        <p className="text-white fw-bold fs-3"> --&#62; Content goes here &#60;-- </p>
+                    </div>
                 </div>
-                <div id='lower' className="row">
-                    boh
+            </section>
+            <section id="backTwo">
+                <div className="container">
+                    <div id='lower' className="row">
+                        boh
+                    </div>
                 </div>
-            </div>
-        </main>
+            </section>
+        </main >
 
     )
 
